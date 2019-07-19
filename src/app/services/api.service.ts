6e3d29f6751f120dc9d5e92ad47174737b3046b7
Vinson Fernandez <vinson.fernandez@admindirect.com>
2019-07-19 16:45:59 -0700

    feat(apiService): set up post and get requests

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,6 +5,19 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ApiService {
+  private protocol = 'https://';
+  private database = `angular-class-bca1e.firebaseio`;
+  private domain = '.com/';
+  constructor(private http: HttpClient) {
+  }
 
-  constructor(private http: HttpClient) { }
+  private baseUrl() {
+    return this.protocol + this.database + this.domain;
+  }
+  public get(url: string) {
+    return this.http.get(this.baseUrl() + url);
+  }
+  public post(url: string, body: any) {
+    return this.http.post(this.baseUrl() + url, body);
+  }
 }
