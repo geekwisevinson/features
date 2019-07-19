84bd9e4b17b568311ae87fa38a99e1261088e3d0
Vinson Fernandez <vinson.fernandez@admindirect.com>
2019-07-19 15:34:52 -0700

    feat(landingRouting): add html route and component in pages of our feature module

--- src/app/pages/landing/landing.module.ts
+++ src/app/pages/landing/landing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing/landing.component';
 import { LandingRoutingModule } from './landing-routing.module';
+import { HtmlComponent } from './pages/html/html.component';
 
 @NgModule({
   imports: [
     CommonModule,
     LandingRoutingModule,
   ],
-  declarations: [LandingComponent]
+  declarations: [LandingComponent, HtmlComponent]
 })
 export class LandingModule { }
